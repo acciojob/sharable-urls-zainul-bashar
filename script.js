cca696b537a9e568fef38c884ea7d58d5bfe78db
zainul-bashar <120213051+zainul-bashar@users.noreply.github.com>
2023-02-16 23:52:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ function takeInput(event){
 	var inputName=document.getElementById('name').value;
 	var inputYear=document.getElementById('year').value;
 	if(inputName && inputYear){
-		document.getElementById('url').textContent+='name='+inputName+"&"+'year='+inputYear;
+		document.getElementById('url').textContent+='?name='+inputName+"&"+'year='+inputYear;
 		}
 	else if(!inputYear && inputName){
-		document.getElementById('url').textContent+='name='+inputName;
+		document.getElementById('url').textContent+='?name='+inputName;
 	}
 	else{
-		document.getElementById('url').textContent+='year='+inputYear;
+		document.getElementById('url').textContent+='?year='+inputYear;
 	}
 	
 	
