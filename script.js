2c9a4001d4f428864144cedc274cf21813ddffcc
zainul-bashar <120213051+zainul-bashar@users.noreply.github.com>
2023-02-16 21:23:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 // your code here
 var btn=document.getElementById('button');
-btn.addEventListner("click",takeInput);
-function takeInput(){
-	event.preventDefault()
+function takeInput(event){
+	event.preventDefault(event);
 	var inputName=document.getElementById('name').value;
 	var inputYear=document.getElementById('year').value;
 	if(inputName && inputYear){
-		document.getElementById('url').textContent+='Name:'+inputName+'Graduation Year:'+inputYear;
+		document.getElementById('url').textContent+='Name:'+inputName+" "+'Graduation Year:'+inputYear;
 		}
 	else if(!inputYear && inputName){
 		document.getElementById('url').textContent+='Name:'+inputName;
