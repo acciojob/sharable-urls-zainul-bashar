e4b4810b2b7320e1899bb489cf09978bdd0655bb
zainul-bashar <120213051+zainul-bashar@users.noreply.github.com>
2023-02-16 21:17:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // your code here
+var btn=document.getElementById('button');
+btn.addEventListner("click",takeInput);
+function takeInput(){
+	event.preventDefault()
+	var inputName=document.getElementById('name').value;
+	var inputYear=document.getElementById('year').value;
+	if(inputName && inputYear){
+		document.getElementById('url').textContent+='Name:'+inputName+'Graduation Year:'+inputYear;
+		}
+	else if(!inputYear && inputName){
+		document.getElementById('url').textContent+='Name:'+inputName;
+	}
+	else{
+		document.getElementById('url').textContent+='Graduation Year:'+inputYear;
+	}
+	
+	
+}
