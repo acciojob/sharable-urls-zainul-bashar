005b0d1aab1f72a561bb13618fbb3838943c0295
zainul-bashar <120213051+zainul-bashar@users.noreply.github.com>
2023-02-16 23:48:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 // your code here
 function takeInput(event){
 	event.preventDefault(event);
+	console.log(event);
 	var inputName=document.getElementById('name').value;
 	var inputYear=document.getElementById('year').value;
 	if(inputName && inputYear){
-		document.getElementById('url').textContent+='Name:'+inputName+" "+'Graduation Year:'+inputYear;
+		document.getElementById('url').textContent+='name:'+inputName+"&"+'year:'+inputYear;
 		}
 	else if(!inputYear && inputName){
-		document.getElementById('url').textContent+='Name:'+inputName;
+		document.getElementById('url').textContent+='name:'+inputName;
 	}
 	else{
-		document.getElementById('url').textContent+='Graduation Year:'+inputYear;
+		document.getElementById('url').textContent+='year:'+inputYear;
 	}
 	
 	
